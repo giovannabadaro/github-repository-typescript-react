d9bdfc121eee6a3bb9be1b6022d7f0ceaeabfe69
Giovanna <giovannabadaroalves@gmail.com>
2020-08-06 01:54:42 -0300

    style dachboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import { Title, Form, Repositories } from './styles';
 
@@ -21,7 +22,50 @@ const Dashboard: React.FC = () => (
     </Form>
 
     <Repositories>
+      <a href="teste">
+        <img
+         src="https://avatars3.githubusercontent.com/u/29736189?s=400&u=698ae6f217cb4fe0aeacd72f7cf2331a0c0527a1&v=4" 
+         alt="Giovanna"
+        />
+        <div>
+          <strong>giovannabadaro/machine-learning-brazilian-cities</strong>
+          <p>
+            A collection of 79 attributes from Brazilian Cities. 
+            The implementation does a simple exploration about the dataset and
+            use random forest to discover some informations</p>
+        </div>
+        <FiChevronRight size={30} />
+      </a>
 
+      <a href="teste">
+        <img
+         src="https://avatars3.githubusercontent.com/u/29736189?s=400&u=698ae6f217cb4fe0aeacd72f7cf2331a0c0527a1&v=4" 
+         alt="Giovanna"
+        />
+        <div>
+          <strong>giovannabadaro/machine-learning-brazilian-cities</strong>
+          <p>
+            A collection of 79 attributes from Brazilian Cities. 
+            The implementation does a simple exploration about the dataset and
+            use random forest to discover some informations</p>
+        </div>
+        <FiChevronRight size={30} />
+      </a>
+
+      <a href="teste">
+        <img
+         src="https://avatars3.githubusercontent.com/u/29736189?s=400&u=698ae6f217cb4fe0aeacd72f7cf2331a0c0527a1&v=4" 
+         alt="Giovanna"
+        />
+        <div>
+          <strong>giovannabadaro/machine-learning-brazilian-cities</strong>
+          <p>
+            A collection of 79 attributes from Brazilian Cities. 
+            The implementation does a simple exploration about the dataset and
+            use random forest to discover some informations</p>
+        </div>
+        <FiChevronRight size={30} />
+      </a>
     </Repositories>
   </>
 );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
+
 export const Title = styled.h1`
   font-size: 48px;
   color: #3A3A3A;
@@ -45,7 +46,58 @@ export const Form = styled.form`
 `;
 
 export const Repositories = styled.div`
-  /* ... */
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.4s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img{
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div{
+      margin-left: 16px;
+
+      strong{
+        font-size: 20px;
+        color: #3D3D4D;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+      width: 30px;
+      height: 30px;
+    }
+
+  }
 `;
 
 
